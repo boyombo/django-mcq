f9ed4013f5219aeb6aae0d04000eab71e2c92c23
bayo <bayokrapht@gmail.com>
2016-04-09 18:49:08 +0100

    models for question app

--- mcq/question/models.py
+++ mcq/question/models.py
@@ -1,5 +1,38 @@
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Batch(models.Model):
+    name = models.CharField(max_length=200)
+    uploaded_on = models.DateTimeField(default=datetime.now)
+    category = models.ForeignKey(Category)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=250)
+    batch = models.ForeignKey(Batch)
+
+    def __unicode__(self):
+        return self.text
+
+
+class Option(models.Model):
+    label = models.PositiveIntegerField()
+    text = models.CharField(max_length=250)
+    correct = models.BooleanField()
+    question = models.ForeignKey(Question, related_name='answers')
+
+    def __unicode__(self):
+        return self.text
