6f9050aaef9188fe9f247207ee5cdec4c7839ad8
bayo <bayokrapht@gmail.com>
2016-04-10 12:49:57 +0100

    templates for home and batch with tests

--- mcq/question/tests/test_views.py
+++ mcq/question/tests/test_views.py
@@ -1,5 +1,7 @@
-import pytest
+def test_request(client):
+    response = client.get('/question/batch/')
 
-
-def test_nothing():
-    assert 1 < 2
+    assert response.status_code == 200
+    template_names = [i.name for i in response.templates]
+    assert 'question/batch.html' in template_names
+    assert 'base.html' in template_names
--- mcq/question/views.py
+++ mcq/question/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def new_batch(request):
+    return render(request, 'question/batch.html', {})
